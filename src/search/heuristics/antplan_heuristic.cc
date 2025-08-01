9bf67208768d2cd1daf33dc8f2b92b35b10aa90e
Roshan Dhakal <dhakalrosan@gmail.com>
2025-08-01 14:50:52 -0400

    added back ff to antplan

--- src/search/heuristics/antplan_heuristic.cc
+++ src/search/heuristics/antplan_heuristic.cc
@@ -125,7 +125,7 @@ int AntPlanHeuristic::compute_heuristic(const State &ancestor_state) {
         utils::g_log << "[AntPlan] Python function failed: " << e.what() << endl;
     }
 
-    int total_h = anticipatory_cost;
+    int total_h = anticipatory_cost + h_ff;
 
     // ✅ Debug Output
     utils::g_log << "\n[AntPlan] Current State Heuristic:" << endl;
